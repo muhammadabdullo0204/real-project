deead66c296a6e2d1fc50bb4e5d8d9ed75759445
Muhammadabdullo Ziyodulloyev <muhammadabdullo0204@gmail.com>
2024-04-20 20:57:30 +0500

    almost-done

---
+++ jquery-work.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".analysis").click(function(){
+      $(".technology-index-box-text-p").text("In spite of disturbing the agile development process of the DevOps approach, the additional security inputs only enhance the safeguards and reliability of the project. So, in a way, DevSecOps will only extend the benefits of a DevOps approach further with security inputs.");
+    });
+});
+
+$(document).ready(function(){
+    $(".design").click(function(){
+      $(".technology-index-box-text-p").text("In spite of disturbing the agile development process of the DevOps approach, the additional security inputs only enhance the safeguards and reliability of the project.");
+    });
+});
+
+$(document).ready(function(){
+    $(".testing").click(function(){
+      $(".technology-index-box-text-p").text("In spite of disturbing the agile development process of the DevOps approach, the additional security inputs only enhance the safeguards and reliability of the project. So, in a way.");
+    });
+});
